e3dfe5c841f801e7b76a790851b25c731b321a80
Anderson Leon <leon.jass@outlook.com>
2023-11-11 15:49:34 -0600

    form termiado de la semana 5 tarea 5

--- tarea5Semana5/JavaApplication82/src/javaapplication82/productosFor.java
+++ tarea5Semana5/JavaApplication82/src/javaapplication82/productosFor.java
@@ -1,16 +1,68 @@
 
 package javaapplication82;
 import  javax.swing.table.DefaultTableModel;
+import  javax.swing.table.DefaultTableModel;
 
 public class productosFor extends javax.swing.JFrame {
     
     DefaultTableModel dtm = new DefaultTableModel();
+    public static int NR=5; //numero de productos introducidos
     
     public productosFor() {
         initComponents();
         String[] titulo = {"CodProd", "Producto", "Cantidad", "Precio", "Grab"};
         dtm.setColumnIdentifiers(titulo);
         jTable1.setModel(dtm);
+        listar();
+    }
+    
+    public void AgregarRenglones(){
+       for(int k=0; k<NR; k++)
+        dtm.addRow(new Object[]{"","","","",""});
+    }
+    
+    public void EliminarRenglones(){
+        int nreng = dtm.getRowCount();
+        if(nreng>0){
+          for(int k=1; k<=nreng; k++)  
+            dtm.removeRow(0);
+        }    
+    }
+    
+    public void listar(){
+        EliminarRenglones();
+        AgregarRenglones();
+        dtm.setValueAt("P01", 0, 0);
+        dtm.setValueAt("P02", 1, 0);
+        dtm.setValueAt("P03", 2, 0);
+        dtm.setValueAt("P04", 3, 0);
+        dtm.setValueAt("P05", 4, 0);
+        
+        dtm.setValueAt("QUESO", 0, 1);
+        dtm.setValueAt("PAPAS", 1, 1);
+        dtm.setValueAt("MARGARINA", 2, 1);
+        dtm.setValueAt("HUEVOS", 3, 1);
+        dtm.setValueAt("CHILE (4OZ)", 4, 1);
+        dtm.setValueAt("CHILE (4OZ)", 4, 1);
+        
+        dtm.setValueAt(3, 0, 2);
+        dtm.setValueAt(2, 1, 2);
+        dtm.setValueAt(1, 2, 2);
+        dtm.setValueAt(2, 3, 2);
+        dtm.setValueAt(3, 4, 2);
+        
+        dtm.setValueAt(47.50, 0, 3);
+        dtm.setValueAt(8.45, 1, 3);
+        dtm.setValueAt(9.30, 2, 3);
+        dtm.setValueAt(45.00, 3, 3);
+        dtm.setValueAt(7.20, 4, 3);
+        
+        dtm.setValueAt(false, 0, 4);
+        dtm.setValueAt(false, 1, 4);
+        dtm.setValueAt(true, 2, 4);
+        dtm.setValueAt(false, 3, 4);
+        dtm.setValueAt(true, 4, 4);
+        
     }
     
     @SuppressWarnings("unchecked")
@@ -19,6 +71,9 @@ public class productosFor extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -35,25 +90,72 @@ public class productosFor extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(jTable1);
 
+        jButton1.setText("Agregar Renglones");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Eliminar Renglones");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setText("Listar");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(102, 102, 102)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(108, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 433, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(169, 169, 169)
+                        .addComponent(jButton3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(122, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(110, Short.MAX_VALUE))
+                .addGap(6, 6, 6)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 350, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2)
+                    .addComponent(jButton3))
+                .addContainerGap(158, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        AgregarRenglones();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        EliminarRenglones();
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        listar();
+    }//GEN-LAST:event_jButton3ActionPerformed
 
 
     public static void main(String args[]) {
@@ -90,6 +192,9 @@ public class productosFor extends javax.swing.JFrame {
     
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
